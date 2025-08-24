2272aae0314cfb4c4951387aed9ce9aa5c0ec273
victor23k <victorfdez.f@gmail.com>
2025-08-24 20:02:40 +0200

    add mappings for lua exec and go to url

--- nvim/lua/config/mappings.lua
+++ nvim/lua/config/mappings.lua
@@ -7,10 +7,16 @@ local M = {}
       end
       vim.api.nvim_set_keymap(mode, lhs, rhs, options)
   end
-  
+
   function M.globalMappings()
-    M.map('i', 'jk', '<Esc>', { noremap = true, silent = true })			-- remap jj to escape insert mode
-    M.map('v', '<leader>p', '\"_dP') 
+    M.map('i', 'jk', '<Esc>', { noremap = true, silent = true }) -- remap jk to escape insert mode
+    M.map('v', '<leader>p', '\"_dP')
   end
+
+  vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<CR>")
+  vim.keymap.set("n", "<leader>ou", ":Gtu<CR>", { desc = "[O]pen [U]rl" } )
+  vim.keymap.set("n", "<space>x", ":.lua<CR>")
+  vim.keymap.set("v", "<space>x", ":lua<CR>")
+  vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>")
 
 return M
